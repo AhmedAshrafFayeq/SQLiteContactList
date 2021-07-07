00435806a19caa88aa8c284be33c264d8f2ffbc9
Ahmed Fayek <ahmed.fayek@gizasystems.com>
2021-07-07 13:15:58 +0200

    Initial Commit

---
+++ ContactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Ahmed Fayek on 7/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
