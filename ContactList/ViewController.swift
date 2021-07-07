7abd766683d2b3945dc231d73be13da3d94473d6
Ahmed Fayek <ahmed.fayek@gizasystems.com>
2021-07-07 13:28:17 +0200

    add sql and open connection

--- ContactList/ViewController.swift
+++ ContactList/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import SQLite3
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        openDatabaseConnection()
     }
-
+    
+    func openDatabaseConnection() -> OpaquePointer? {
+        var db: OpaquePointer?
+        let fileUrl = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent("Contacts.sqlite")
+        if sqlite3_open(fileUrl?.path, &db) == SQLITE_OK {
+            print("Successfully opened connection to database")
+            return db
+        }else{
+            print("unable to open database")
+            return nil
+        }
+    }
 
 }
 
